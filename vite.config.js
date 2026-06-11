6431f66488c2b188c992159995668d8afa66813b
Zorigo <zorigo.enkhtur@gmail.com>
2026-06-11 15:41:09 +0200

    Tailwind Content-Pfade hinzugefügt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/test-portfolio/",
   plugins: [tailwindcss()],
+  content: ["./index.html", "./impressum.html", "./src/**/*.{js,ts,jsx,tsx}"],
 });
